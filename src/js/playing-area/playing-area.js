13843b9a6c3a8382497317611c388d2303ebd97b
Dmitry-Baz <151816542+Dmitry-Baz@users.noreply.github.com>
2025-12-27 18:08:06 +0400

    Update playing-area.js

--- src/js/playing-area/playing-area.js
+++ src/js/playing-area/playing-area.js
@@ -1,94 +1,38 @@
 export default class PlayingArea {
-  constructor(container, hitsElement, missesElement) {
+  constructor(container, size = 4) {
     this.container = container;
-    this.hitsElement = hitsElement;   
-    this.missesElement = missesElement; 
-    this.intervalId = null;
-    this.hits = 0;
-    this.misses = 0;
-
-    if (!container) {
-      throw new Error('PlayingArea: container not found');
-    }
-
+    this.size = size; // например, 4x4 = 16 ячеек
+    this.totalCells = size * size;
     this.init();
   }
 
   init() {
     this.container.innerHTML = '';
-    this.hits = 0;
-    this.misses = 0;
-    this.updateScore();
-
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < this.totalCells; i++) {
       const item = document.createElement('li');
       item.className = 'playing-area__item';
       item.dataset.index = i;
-      item.addEventListener('click', () => this.handleCellClick(i));
-      this.container.append(item); // используем append
+      this.container.appendChild(item);
     }
   }
 
-  handleCellClick(clickedIndex) {
-    const items = this.container.querySelectorAll('.playing-area__item');
-    const hasGoblin = items[clickedIndex].querySelector('.goblin');
-
-    if (hasGoblin) {
-      this.hits++;
-    } else {
-      this.misses++;
-    }
-
-    this.updateScore();
+  showGoblin(index) {
+    this.hideAll();
+    const cell = this.container.children[index];
+    if (cell) cell.classList.add('goblin');
   }
 
-  updateScore() {
-    if (this.hitsElement) this.hitsElement.textContent = this.hits;
-    if (this.missesElement) this.missesElement.textContent = this.misses;
-  }
-
-  randomMovingGoblin = () => {
-    const items = this.container.querySelectorAll('.playing-area__item');
-
-    
-    items.forEach(item => {
-      const img = item.querySelector('.goblin-img');
-      if (img) item.removeChild(img);
+  hideAll() {
+    this.container.querySelectorAll('.goblin').forEach(el => {
+      el.classList.remove('goblin');
     });
-
-    
-    let randomIndex;
-    do {
-      randomIndex = Math.floor(Math.random() * items.length);
-    } while (items[randomIndex].querySelector('.goblin-img'));
-
-    
-    const img = document.createElement('img');
-    img.src = '../../img/goblin.png'; 
-    img.alt = 'Гоблин';
-    img.className = 'goblin-img'; 
-    img.style.width = '80%';
-    img.style.height = 'auto';
-
-    items[randomIndex].append(img); 
-  };
-
-  start() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-    this.intervalId = setInterval(this.randomMovingGoblin, 1000);
   }
 
-  stop() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
+  getCell(index) {
+    return this.container.children[index];
+  }
 
-    
-    this.container.querySelectorAll('.goblin-img').forEach(img => {
-      img.remove();
-    });
+  get total() {
+    return this.totalCells;
   }
 }
